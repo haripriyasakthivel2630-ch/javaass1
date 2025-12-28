1979d0291a6b3f64a678f54d439f080953b47ecd
haripriyasakthivel2630-ch <haripriyasakthivel2@gmail.com>
2025-12-28 21:07:19 +0530

    Add files via upload

---
+++ Assignment1_java.java
@@ -0,0 +1,17 @@
+package com.index;
+
+public class Assignment1_java {
+
+	public static void main(String[] args) {
+		
+		int marks=45;
+		if(marks>=40) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Fail");
+		}
+		
+
+	}
+
+}
