323c480c075d5a17203d2c4f3ceb5a7492246164
haripriyasakthivel2630-ch <haripriyasakthivel2@gmail.com>
2025-12-28 21:07:43 +0530

    Add files via upload

---
+++ Assignment1_Ifjava.java
@@ -0,0 +1,17 @@
+package com.index;
+
+public class Assignment1_Ifjava {
+
+	public static void main(String[] args) {
+		
+		int number=10;
+		if(number % 2==0) {
+			System.out.println("This is a Even number");
+		}
+		else {
+			System.out.println("This is a Odd number");
+		}
+
+	}
+
+}
