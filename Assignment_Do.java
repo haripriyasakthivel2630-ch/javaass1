fe577375beb529449d43052a5213a8f519c5203b
haripriyasakthivel2630-ch <haripriyasakthivel2@gmail.com>
2025-12-31 19:58:23 +0530

    Add files via upload

--- Assignment_Do.java
+++ Assignment_Do.java
@@ -1,10 +1,16 @@
 package com.index;
 
+import java.util.Scanner;
+
 public class Assignment_Do {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		String yn;
+		do {
+			System.out.println("Do you want to continue?(y/n)");
+		}
+        while(yn.equals("y"));
 	}
 
 }
