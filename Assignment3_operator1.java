426aa817650cb8f5c40784e4a35a3a524298edfc
haripriyasakthivel2630-ch <haripriyasakthivel2@gmail.com>
2025-12-31 20:37:12 +0530

    Add files via upload

---
+++ Assignment3_operator1.java
@@ -0,0 +1,15 @@
+package com.index;
+import java.util.Scanner;
+
+public class Assignment3_operator1 {
+
+	public static void main(String[] args) {
+		
+            for(int i=0;1<=10;i++) {
+            	System.out.println("Increment number:"+i);
+             
+            	
+            }
+	}
+
+}
