982342d792ab0b33c391239777ae2072b3ba3712
haripriyasakthivel2630-ch <haripriyasakthivel2@gmail.com>
2026-01-04 21:24:27 +0530

    Add files via upload

---
+++ Assignment4_static.java
@@ -0,0 +1,16 @@
+package com.index;
+
+import java.util.Scanner;
+
+public class Assignment4_static {
+	static int number;
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		number=sc.nextInt();
+		System.out.println("static variable value:"+staticvariableScanner.number); 
+
+	}
+
+}
