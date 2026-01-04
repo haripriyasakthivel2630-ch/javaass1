66514b23439a4e9dfdb3fbf093293fba2e809a12
haripriyasakthivel2630-ch <haripriyasakthivel2@gmail.com>
2026-01-04 16:50:33 +0530

    Add files via upload

---
+++ Assignment_local.java
@@ -0,0 +1,18 @@
+package com.index;
+
+import java.util.Scanner;
+
+public class Assignment_local {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int a=sc.nextInt();
+		System.out.println("Enter the second number");
+		int b=sc.nextInt();
+		int sum=a+b;
+		System.out.println("Sum of two number"+sum);
+
+	}
+
+}
