ee340692092c3b4471a4b664e9f7607f78740ee3
haripriyasakthivel2630-ch <haripriyasakthivel2@gmail.com>
2026-01-04 21:20:37 +0530

    Add files via upload

---
+++ Assignment4_java5.java
@@ -0,0 +1,23 @@
+package com.index;
+import java.util.Scanner;
+
+public class Assignment4_java5 {
+	/*
+	 * void show(){
+	 * 
+	 *//Without Initialize a value
+	 int 1;
+	 System.out.println("Without Initializing a value:"+1);
+}
+	 */
+	 //with initialize a value
+	 String local="Local variable with initalizing a value";
+
+	public static void main(String[] args) {
+		Assignment4_java5 Ivd=new Assignment4_java5();
+		//Ivd.show();
+		System.out.println("with Initializing a value:"+Ivd.local);
+
+	}
+
+}
